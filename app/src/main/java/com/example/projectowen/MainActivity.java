911575fd7e438ad799aec32facca3315bff45cf3
Wilson Watson <wilsonwatson@utexas.edu>
2023-03-24 23:27:27 -0500

    android now references common

--- app/src/main/java/com/example/projectowen/MainActivity.java
+++ app/src/main/java/com/example/projectowen/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.projectowen;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import net.ffst.adbpotato.Bridge;
+
 public class MainActivity extends AppCompatActivity {
 
-    protected void onCreate(Bundle savedInstanceS
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        Bridge bridge = new Bridge();
     }
 
 }
